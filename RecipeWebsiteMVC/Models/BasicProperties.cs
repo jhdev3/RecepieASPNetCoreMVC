79d925269e9eaafd62b1dfbdb8a96f79a2361cb5
jhdev3 <jacobheden@gmail.com>
2022-02-28 13:19:03 +0100

    Update BasicProperties.cs

--- RecipeWebsiteMVC/Models/BasicProperties.cs
+++ RecipeWebsiteMVC/Models/BasicProperties.cs
@@ -2,13 +2,17 @@
 {
     /// <summary>
     /// Classes/models that want to store in a db should have theese properties
-    /// Using new syntax instead of an construktio to initalize them :) 
-    /// ID is to easy search etc and DateOfCreation can be used for error check but also if we want to remove objects from a database based on how long its been stored :). 
- 
+    /// Using new syntax instead of an constructor to initalize them :)  
     /// </summary>
     public abstract class BasicProperties
     {
+        /// <summary>
+        /// Id of an object 
+        /// </summary>
         public string Id { get;} = Guid.NewGuid().ToString();
+        /// <summary>
+        /// Sets the date of creation to right now 
+        /// </summary>
         public DateTime DateOfCreation { get; set; } = DateTime.Now;
 
     }
