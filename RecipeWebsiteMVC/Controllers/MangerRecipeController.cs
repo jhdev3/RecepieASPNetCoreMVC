d3cf51fdb1e5cb120e75d226d6f3018321109c37
Jacob Hedén <91886824+jhdev3@users.noreply.github.com>
2022-03-02 11:22:46 +0100

    From Scratch

---
+++ RecipeWebsiteMVC/Controllers/MangerRecipeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeWebsiteMVC.Controllers
+{
+    public class MangerRecipeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
