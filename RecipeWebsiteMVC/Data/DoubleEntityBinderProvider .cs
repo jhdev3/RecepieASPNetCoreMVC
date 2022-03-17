1422fbaea5248fc22ef70474e5793ac44af23d9f
Jacob Hedén <91886824+jhdev3@users.noreply.github.com>
2022-03-17 23:24:54 +0100

    Done
    And Done

---
+++ RecipeWebsiteMVC/Data/DoubleEntityBinderProvider .cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace RecipeWebsiteMVC.Data
+{
+    public class DoubleEntityBinderProvider : IModelBinderProvider
+    {
+        /// <summary>
+        /// Binder overrider för double så lägga till i program.cs 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+          
+              if (context == null)
+                {
+                    throw new ArgumentNullException(nameof(context));
+                }
+
+                if (context.Metadata.ModelType == typeof(double))
+                {
+                    return new BinderTypeModelBinder(typeof(CostumDoubleModelBinder));
+                }
+
+                return null;
+            
+        }
+    }
+}
